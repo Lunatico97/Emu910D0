fef40341fdca1fac4850fa1a0c1f334387a05608
diwas <lunaticoda123@gmail.com>
2026-04-19 21:56:43 +0545

    configured nmi suppression  on vblank read and nes pipeline fix

--- src/gui.cpp
+++ src/gui.cpp
@@ -66,23 +66,26 @@ void GUI::run_nes()
 
         if((nes_state->system_clock % 3) == 0)
         {
+            nes_state->apu->clock_apu_fcnt();
             if(nes_state->mmu->dma_rqst) 
                 nes_state->mmu->perform_dma(nes_state->system_clock % 2);
             else if(nes_state->apu->refDMC->buf_empty && nes_state->apu->refDMC->byte_rem > 0x00)
                 nes_state->mmu->perform_audio_dma(nes_state->system_clock % 2);
-            else nes_state->cpu->clock();
-            nes_state->apu->clock_apu_fcnt();
-        }
+            else
+            {
+                nes_state->cpu->clock();
 
-        if(nes_state->ppu->trigger_nmi)
-        {
-            nes_state->cpu->nmi();
-            nes_state->ppu->trigger_nmi = false;
-        }
-
-        if(nes_state->apu->frame_irq || nes_state->crom->map_irq || nes_state->mmu->dma_irq) 
-        {
-            nes_state->cpu->irq(); 
+                if(nes_state->ppu->trigger_nmi)
+                {
+                    nes_state->cpu->nmi();
+                    nes_state->ppu->trigger_nmi = false;
+                }
+                
+                if(nes_state->apu->frame_irq || nes_state->crom->map_irq || nes_state->mmu->dma_irq) 
+                {
+                    nes_state->cpu->irq(); 
+                }
+            } 
         }
 
         nes_state->system_clock++;
